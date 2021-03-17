cd9d69314d77d3299b4eee33571f329f4d7f802c
Lori Segovia <segovialori@gmail.com>
2021-03-16 22:35:10 -0500

    functions to evaluate

---
+++ evaluate.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import f_regression 
+from math import sqrt
+import math
+import matplotlib.pyplot as plt
+%matplotlib inline
+import warnings
+warnings.filterwarnings('ignore')
+
+#function to plot residuals 
+def plot_residuals(actual, predicted):
+    residuals = actual - predicted
+    plt.hlines(0, actual.min(), actual.max(), ls=':')
+    plt.scatter(actual, residuals)
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title('Actual vs Residual')
+    plt.show()
+
+def residuals(actual, predicted):
+    return actual - predicted
+
+def sse(actual, predicted):
+    return (residuals(actual, predicted) **2).sum()
+
+def mse(actual, predicted):
+    n = actual.shape[0]
+    return sse(actual, predicted) / n
+
+def rmse(actual, predicted):
+    return math.sqrt(mse(actual, predicted))
+
+def ess(actual, predicted):
+    return ((predicted - actual.mean()) ** 2).sum()
+
+def tss(actual):
+    return ((actual - actual.mean()) ** 2).sum()
+
+
+def regression_errors(actual, predicted):
+    return pd.Series({
+        'sse': sse(actual, predicted),
+        'ess': ess(actual, predicted),
+        'tss': tss(actual),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    })
+
+def baseline_mean_errors(actual):
+    predicted = actual.mean()
+    return {
+        'sse': sse(actual, predicted),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    }
+
+def better_than_baseline(actual, predicted):
+    rmse_baseline = rmse(actual, actual.mean())
+    rmse_model = rmse(actual, predicted)
+    return rmse_model < rmse_baseline
+
+def model_significance(ols_model):
+    return {
+        'r^2 -- variance explained': ols_model.rsquared,
+        'p-value -- P(data|model == baseline)': ols_model.f_pvalue,
+    }
